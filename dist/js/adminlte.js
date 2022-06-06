65e67dd646302ba68f7193cc7cdb0392832b2fa8
thanhnt6 <thanhnt6@rikkeisoft.com>
2022-06-06 17:17:50 +0700

    deploy

--- dist/js/adminlte.js
+++ dist/js/adminlte.js
@@ -1851,12 +1851,12 @@
       setTimeout(function () {
         var $preloader = $__default["default"](SELECTOR_PRELOADER);
 
-        if ($preloader) {
-          $preloader.css('height', 0);
-          setTimeout(function () {
-            $preloader.children().hide();
-          }, 200);
-        }
+        // if ($preloader) {
+        //   $preloader.css('height', 0);
+        //   setTimeout(function () {
+        //     $preloader.children().hide();
+        //   }, 200);
+        // }
       }, this._config.preloadDuration);
     };
 
